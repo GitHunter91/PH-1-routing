b29667df4f60996298551e60ff0ccbd04dcb6a24
Hunter Sexton <h.sexton@live.com>
2018-03-27 12:33:16 -0700

    add switch functionality

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -5,6 +5,7 @@ import About from './About/About';
 import People from './People/People';
 import FAQ from './About/FAQ/FAQ';
 import Company from './About/Company/Company';
+import Character from './Character/Character';
 
 class App extends Component {
   render() {
@@ -21,7 +22,10 @@ class App extends Component {
             <Route path="/about/faq" component={FAQ}/>
             <Route path="/about/company" component={Company}/>
           </About>}/>
-        <Route path="/people" component={People}/>
+        <Switch>
+        <Route path="/people/:id" component={Character}/>
+        <Route exact path="/people" component={People}/>
+        </Switch>
       </div>
     );
   }
