b831960f22572fb7ba251f54740b92b63ae71259
Hunter Sexton <h.sexton@live.com>
2018-03-27 12:08:16 -0700

    initial morning git - thru about subroutes

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter as R } from 'react-router-dom';
+ReactDOM.render(<R>
+    <App />
+    </R>, document.getElementById('root'));
 // registerServiceWorker();
